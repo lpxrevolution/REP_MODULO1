7c66e1a563f93a13ff019f56fa7561863e9f6eb7
LPxRevolution <rbnrevolution@gmail.com>
2022-02-07 09:17:42 +0100

    Evalable ED

---
+++ PRG/NetBeans/UML_test_NetBeans/src/uml_test_netbeans/UML_test_NetBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uml_test_netbeans;
+
+/**
+ *
+ * @author rbnre
+ */
+public class UML_test_NetBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
